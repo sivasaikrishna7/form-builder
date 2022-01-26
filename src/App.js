40e13b38d796f4081fee3fb925352c7735359bb8
sivasaikrishna7 <sivasaikrishna.n18@iiits.in>
2022-01-26 16:33:20 +0530

    ui updated

--- src/App.js
+++ src/App.js
@@ -1,74 +1,74 @@
-import React, { useState, useEffect } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { v4 as uuid } from "uuid";
-import "./App.css";
-import { motion } from "framer-motion";
-import objData from "./store";
-import { MdDelete } from "react-icons/md";
-import handleSubmit from "./Logic";
-import "./global.css";
-import GetContext from "./components/GetContext";
+import React, { useState, useEffect } from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+import { v4 as uuid } from 'uuid'
+import './App.css'
+import { motion } from 'framer-motion'
+import objData from './store'
+import { MdDelete } from 'react-icons/md'
+import handleSubmit from './Logic'
+import './global.css'
+import GetContext from './components/GetContext'
 
 function App() {
-  const [state, setState] = useState(objData);
-  const [newId, setNewId] = useState(uuid());
-  const [columns, setColumns] = useState([]);
-  const [type, setType] = useState("");
-  const [template_name, setTemplateName] = useState("");
-  const [label, setLabel] = useState("");
-  const [itemsFromBackend, setitemsFromBackend] = useState([]);
+  const [state, setState] = useState(objData)
+  const [newId, setNewId] = useState(uuid())
+  const [columns, setColumns] = useState([])
+  const [type, setType] = useState('')
+  const [template_name, setTemplateName] = useState('')
+  const [label, setLabel] = useState('')
+  const [itemsFromBackend, setitemsFromBackend] = useState([])
 
   const changeLabel = (e) => {
-    setLabel(e.target.value);
-  };
+    setLabel(e.target.value)
+  }
 
   const getLabel = (param) => {
     switch (param) {
-      case "text":
-        return "Text";
-      case "int":
-        return "Int";
+      case 'text':
+        return 'Text'
+      case 'int':
+        return 'Int'
       default:
-        return "";
+        return ''
     }
-  };
+  }
 
   const onChangeHandler = (arg) => {
     if (itemsFromBackend.length !== 0 && state.field.length === 0) {
-      alert("Add your step");
-      return;
+      alert('Add your step')
+      return
     }
-    setType(arg);
-    setitemsFromBackend([...itemsFromBackend, { id: newId, type: arg }]);
-  };
+    setType(arg)
+    setitemsFromBackend([...itemsFromBackend, { id: newId, type: arg }])
+  }
 
   const removeField = (id) => {
-    const filteredPeople = itemsFromBackend.filter((item) => item.id !== id);
-    setitemsFromBackend(filteredPeople);
+    const filteredPeople = itemsFromBackend.filter((item) => item.id !== id)
+    setitemsFromBackend(filteredPeople)
     if (label.length > 0) {
       state.field.splice(
         state.field.findIndex((a) => a.id === id),
-        1
-      );
+        1,
+      )
     }
-  };
+  }
 
   const changeTempName = (e) => {
-    setTemplateName(e.target.value);
-  };
+    setTemplateName(e.target.value)
+  }
 
   const onsubmitHandler = () => {
     if (template_name.length === 0) {
-      alert("Enter the Template Name!");
-      return;
+      alert('Enter the Template Name!')
+      return
     }
     if (itemsFromBackend.length === 0) {
-      alert("Please Add a Field!");
-      return;
+      alert('Please Add a Field!')
+      return
     }
     if (label.length === 0) {
-      alert("Enter the Label");
-      return;
+      alert('Enter the Label')
+      return
     }
     setState({
       template_name: template_name,
@@ -80,39 +80,42 @@ function App() {
           type: type,
         },
       ],
-    });
-    setLabel("");
-    setNewId(uuid());
-  };
+    })
+    setLabel('')
+    setNewId(uuid())
+  }
 
   useEffect(() => {
     setColumns({
       [uuid()]: {
-        name: "Todo",
+        name: 'Todo',
         items: itemsFromBackend,
       },
-    });
-  }, [itemsFromBackend, state]);
+    })
+  }, [itemsFromBackend, state])
 
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
+  // useEffect(() => {
+  //   console.log(state)
+  // }, [state])
 
-  const onhandleSubmit = () => {
-    setTemplateName("");
-    setitemsFromBackend([]);
-    setState(objData);
-    handleSubmit(state);
-  };
+  const onhandleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hi')
+    console.log(state)
+    handleSubmit(state)
+    setTemplateName('')
+    setitemsFromBackend([])
+    setState(objData)
+  }
 
   return (
     <>
       <div className="sidenav">
         <h3>Basic Elements</h3>
-        <button className="button" onClick={() => onChangeHandler("text")}>
+        <button className="button" onClick={() => onChangeHandler('text')}>
           TEXT
         </button>
-        <button className="button" onClick={() => onChangeHandler("int")}>
+        <button className="button" onClick={() => onChangeHandler('int')}>
           INT
         </button>
       </div>
@@ -121,7 +124,7 @@ function App() {
           <motion.div
             initial={{ y: -200 }}
             animate={{ y: 0 }}
-            transition={{ type: "spring", duration: 0.5 }}
+            transition={{ type: 'spring', duration: 0.5 }}
             whileHover={{ scale: 1.1 }}
             className="templateName"
           >
@@ -148,9 +151,9 @@ function App() {
                       >
                         {column.items.map((item, index) => {
                           const isItem = (item1) => {
-                            return item1.id === item.id;
-                          };
-                          const element = state.field.find(isItem);
+                            return item1.id === item.id
+                          }
+                          const element = state.field.find(isItem)
                           return (
                             <Draggable
                               key={item.id}
@@ -176,33 +179,33 @@ function App() {
                                         whileHover={{ scale: 1.4 }}
                                         whileTap={{ scale: 0.9 }}
                                         style={{
-                                          color: "red",
-                                          display: "flex",
-                                          alignItems: "center",
-                                          padding: "5px",
-                                          border: "none",
-                                          borderRadius: "50%",
-                                          cursor: "pointer",
+                                          color: 'red',
+                                          display: 'flex',
+                                          alignItems: 'center',
+                                          padding: '5px',
+                                          border: 'none',
+                                          borderRadius: '50%',
+                                          cursor: 'pointer',
                                         }}
                                         onClick={() => removeField(item.id)}
                                       >
-                                        <MdDelete size={"1.5em"} />
+                                        <MdDelete size={'1.5em'} />
                                       </motion.button>
                                     </div>
                                     <br />
                                   </div>
-                                );
+                                )
                               }}
                             </Draggable>
-                          );
+                          )
                         })}
 
                         {provided.placeholder}
                       </form>
-                    );
+                    )
                   }}
                 </Droppable>
-              );
+              )
             })}
           </DragDropContext>
         </div>
@@ -216,7 +219,7 @@ function App() {
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
